6fd20187abf7fe765f7274f3ca067815a51fdf3c
João Lima <figorifico3000@gmail.com>
2019-03-01 09:48:30 +0000

    spiners

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 let container = document.querySelector("#main");
 console.log(container);
-criar(100);
+criar(10);
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -12,6 +12,32 @@ async function criar(tempo) {
         let novo = document.createElement("div");
         novo.setAttribute("class", "spinner-border");
         container.appendChild(novo);
+        await sleep(tempo);
+        
+        let novo3 = document.createElement("div");
+        novo3.setAttribute("class", "spinner-border text-primary");
+        container.appendChild(novo3);
+        await sleep(tempo);
+        
+        let novo4 = document.createElement("div");
+        novo4.setAttribute("class", "spinner-border text-success");
+        container.appendChild(novo4);
+        await sleep(tempo);
+        let novo5 = document.createElement("div");
+        novo5.setAttribute("class", "spinner-border text-info");
+        container.appendChild(novo5);
+        await sleep(tempo);
+        let novo6 = document.createElement("div");
+        novo6.setAttribute("class", "spinner-border text-warning");
+        container.appendChild(novo6);
+        await sleep(tempo);
+        let novo7 = document.createElement("div");
+        novo7.setAttribute("class", "spinner-border text-danger");
+        container.appendChild(novo7);
+        await sleep(tempo);
+        let novo8 = document.createElement("div");
+        novo8.setAttribute("class", "spinner-border text-secondary");
+        container.appendChild(novo8);
         await sleep(tempo);
     }
 }
