22a6aa63e51a0026bdef38792b9f911602bea07a
Unknown <julianamcm05@gmail.com>
2019-03-19 11:24:32 +0000

    fixed A4

--- database/db.sql
+++ database/db.sql
@@ -4,12 +4,38 @@ CREATE TYPE rankType AS ENUM ('rookie', 'beginner', 'intermediate', 'enthusiasti
 CREATE TYPE roleType AS ENUM ('member','moderator', 'administrator');
 
 --Tables
-CREATE Rank TABLE ();
-CREATE Role TABLE ();
-CREATE User TABLE ();
-CREATE Notification TABLE ();
-CREATE Report TABLE();
-CREATE Question TABLE();
-CREATE Answer TABLE();
-CREATE FAQ TABLE();
-CREATE BestAnswer TABLE();
+CREATE Rank TABLE (
+
+);
+
+CREATE Role TABLE (
+
+);
+
+CREATE User TABLE (
+
+);
+
+CREATE Notification TABLE (
+
+);
+
+CREATE Report TABLE(
+
+);
+
+CREATE Question TABLE(
+
+);
+
+CREATE Answer TABLE(
+
+);
+
+CREATE FAQ TABLE(
+
+);
+
+CREATE BestAnswer TABLE(
+
+);
