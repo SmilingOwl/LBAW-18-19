8eb44d302e31c5d8b6316bb3f12d145c4a185307
Unknown <julianamcm05@gmail.com>
2019-03-19 09:31:44 +0000

    added database folder

---
+++ database/db.sql
@@ -0,0 +1,15 @@
+--Types
+CREATE TYPE notificationType AS ENUM ('question', 'answer', 'comment', 'follow', 'vote');
+CREATE TYPE rankType AS ENUM ('rookie', 'beginner', 'intermediate', 'enthusiastic', 'adanced', 'veteran');
+CREATE TYPE roleType AS ENUM ('member','moderator', 'administrator');
+
+--Tables
+CREATE Rank TABLE ();
+CREATE Role TABLE ();
+CREATE User TABLE ();
+CREATE Notification TABLE ();
+CREATE Report TABLE();
+CREATE Question TABLE();
+CREATE Answer TABLE();
+CREATE FAQ TABLE();
+CREATE BestAnswer TABLE();
