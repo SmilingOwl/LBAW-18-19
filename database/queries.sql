1197f2a1784f4fb39a87e655ffcfbf654d3dad04
Unknown <julianamcm05@gmail.com>
2019-04-03 21:38:43 +0100

    added some queries!

--- database/queries.sql
+++ database/queries.sql
@@ -1,40 +1,59 @@
 ---------------QUERIES---------------
 
 -- Obtain a member’s profile information
-
 SELECT username, bioDescription, points, profilePhoto, rankType
 FROM "user", rank
 Where  "user".id_rank=rank.id_rank; 
 
 -- Obtain a member´s number of followers 
-
 SELECT COUNT (*) as number_of_followers
 FROM follow INNER JOIN "user" ON follow.following ="user".id_user;
 
 -- Obtain a member´s number of following
-
 SELECT COUNT (*) as number_of_following
 FROM follow INNER JOIN "user" ON follow.followers ="user".id_user;
 
 -- Obtain a member’s most recent questions (limit 10)
+SELECT title, description, date
+FROM question INNER JOIN "user" ON question.id_user = user.id_user
+ORDER BY date DESC
+LIMIT 10;
 
 -- Obtain a member’s most recent answers
+SELECT text, date
+FROM answer INNER JOIN "user" ON answer.user_post = "user".id_user
+ORDER BY date DESC
+LIMIT 10;
 
 -- Obtain a member’s followers
 
 -- Obtain a member’s following
 
 -- Obtain a member’s notifications
+SELECT type, description, date, view
+FROM notification INNER JOIN "user" ON notification.id_user = "user".id_user
+ORDER BY date DESC;
+
 
 -- Obtain a question’s details
+SELECT "user".username, "user".profile_picture, question.name, question.description, question.date, question.votes
+FROM question INNER JOIN "user" ON question.id_user = "user".id_user;
+
 
 -- Obtain all the answers to a question
+SELECT "user".username, "user".profile_picture, answer.description, answer.date
+FROM answer INNER JOIN question ON answer.id_question = question.id_question INNER JOIN "user" ON question.id_user = "user".id_user;
 
 -- Obtain all the comments to an answer
+
 
 -- Obtain questions matching a search input
 
 -- Obtain the top users (highest score)
+SELECT username, profile_picture, points
+FROM "user"
+ORDER BY points DESC
+LIMIT 10;
 
 -- Obtain trending category (category with the most questions associated)
 
