32d6e66a6f79685386b65b107d22219440607e97
Unknown <julianamcm05@gmail.com>
2019-04-04 10:45:25 +0100

    added more queries

--- database/queries.sql
+++ database/queries.sql
@@ -5,13 +5,16 @@ SELECT username, bioDescription, points, profilePhoto, rankType
 FROM "user", rank
 Where  "user".id_rank=rank.id_rank; 
 
+
 -- Obtain a member´s number of followers 
 SELECT COUNT (*) as number_of_followers
 FROM follow INNER JOIN "user" ON follow.following ="user".id_user;
 
+
 -- Obtain a member´s number of following
 SELECT COUNT (*) as number_of_following
 FROM follow INNER JOIN "user" ON follow.followers ="user".id_user;
+
 
 -- Obtain a member’s most recent questions (limit 10)
 SELECT title, description, date
@@ -19,41 +22,50 @@ FROM question INNER JOIN "user" ON question.id_user = user.id_user
 ORDER BY date DESC
 LIMIT 10;
 
+
 -- Obtain a member’s most recent answers
 SELECT text, date
 FROM answer INNER JOIN "user" ON answer.user_post = "user".id_user
 ORDER BY date DESC
 LIMIT 10;
 
+
 -- Obtain a member’s followers
+SELECT username , profilePhoto , points, id_rank 
+FROM follow INNER JOIN "user" ON  follow.following = "user".id_user;
+
 
 -- Obtain a member’s following
+SELECT username , profilePhoto , points, id_rank 
+FROM follow INNER JOIN "user" ON  follow.followers = "user".id_user;
+
 
 -- Obtain a member’s notifications
-SELECT type, description, date, view
+SELECT type, description, "date", view
 FROM notification INNER JOIN "user" ON notification.id_user = "user".id_user
-ORDER BY date DESC;
+ORDER BY "date" DESC;
 
 
 -- Obtain a question’s details
-SELECT "user".username, "user".profile_picture, question.name, question.description, question.date, question.votes
+SELECT "user".username, "user".profilePhoto, question.name, question.description, question.date, question.votes
 FROM question INNER JOIN "user" ON question.id_user = "user".id_user;
 
 
 -- Obtain all the answers to a question
-SELECT "user".username, "user".profile_picture, answer.description, answer.date
+SELECT "user".username, "user".profilePhoto, answer.description, answer.date
 FROM answer INNER JOIN question ON answer.id_question = question.id_question INNER JOIN "user" ON question.id_user = "user".id_user;
 
--- Obtain all the comments to an answer
-
-
--- Obtain questions matching a search input
 
 -- Obtain the top users (highest score)
-SELECT username, profile_picture, points
+SELECT username, profilePhoto, points
 FROM "user"
 ORDER BY points DESC
 LIMIT 10;
+
+
+-- Obtain all the comments to an answer
+
+-- Obtain questions matching a search input
 
 -- Obtain trending category (category with the most questions associated)
 
@@ -63,14 +75,27 @@ LIMIT 10;
 --------------UPDATES--------------
  
 -- Update user info
+UPDATE "user"
+SET username = $name, password = $password, photo = $photo,
+bioDescription = $bioDescription, profilePhoto = $profilePhoto,
+points = $points;
+
 
 -- Update question
+UPDATE question
+SET title = $title , description = $description, name = $name;
+
 
 -- Update answer
+UPDATE answer
+SET "text" = $"text", photo = $photo;
 
--- Update comment
 
 -- Mark notification as read
+UPDATE notification
+SET view = true;
+
+-- Update comment
 
 -- rate a question
 
