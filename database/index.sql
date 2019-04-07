f1a464966260d58d72bad484f8bd1b3100af649f
leonormfreitas <leonormendesfreitas@gmail.com>
2019-04-07 16:16:01 +0100

    Update index.sql

--- database/index.sql
+++ database/index.sql
@@ -1,3 +1,26 @@
+--INDEXES ON MOST USED TABLES
+
+--SELECT01 (user profile) , HIGH CARDINALITY, NO CLUSTERING
+
+CREATE INDEX usernames ON "user" USING hash (username);
+
+--SELECT09 (quesTion details), MEDIUM CARDINALITY, CLUSTERING
+
+CREATE INDEX details ON "question" USING hash (id_user);
+
+--SELECT10 (question answers), MEDIUM CARDINALITY, CLUSTERING
+
+CREATE INDEX answers ON "answer" USING hash (id_question);
+
+--SELECT13 (question by date), LOW CARDINALITY, NO CLUSTERING
+
+CREATE INDEX question_date ON "question" USING btree ("date");
+
+--SELECT14 (comments), HIGH CARDINALITY, CLUSTERING
+
+CREATE INDEX comments ON "comment" USING hash (firstAnswer);
+
+
 
 --GiST because it's better for dynamic data.
 CREATE INDEX search_idx ON question USING GIST (search);
