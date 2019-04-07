eb7bcac49ec809c6cfd6cdb1e39a4260d373ed1e
Unknown <julianamcm05@gmail.com>
2019-04-07 12:59:32 +0100

    organized

--- database/queries.sql
+++ database/queries.sql
@@ -1,4 +1,6 @@
----------------QUERIES---------------
+-----------------------------------------
+--QUERIES
+-----------------------------------------
 
 -- Obtain a member’s profile information
 SELECT username, bioDescription, points, profilePhoto, name
@@ -23,7 +25,7 @@ SELECT COUNT (*) as number_of_following
 FROM follow 
 WHERE follow.follower = $id_user;
 
--- Obtain a member´s name of following
+-- Obtain a member´s following
 SELECT id_user,username , profilePhoto , points, id_rank 
 FROM follow INNER JOIN "user" ON follow.following ="user".id_user
 WHERE follow.follower = $id_user;
@@ -36,7 +38,7 @@ ORDER BY date DESC
 LIMIT 10;
 
 
--- Obtain a member’s most recent answers
+-- Obtain a member’s most recent answers (limit 10)
 SELECT text, date
 FROM answer
 WHERE answer.user_post = $id_user
@@ -73,10 +75,10 @@ LIMIT 10;
 -- Obtain trending category (category with the most questions associated)
 SELECT name
 FROM 
-(SELECT category.name AS name, COUNT(id_question) AS quantidade
-FROM category INNER JOIN question ON (category.id_category = question.id_category)
-WHERE question."date" BETWEEN NOW() - INTERVAL '1 DAY' AND now()
-GROUP BY  category.name) AS sub
+    (SELECT category.name AS name, COUNT(id_question) AS quantidade
+    FROM category INNER JOIN question ON (category.id_category = question.id_category)
+    WHERE question."date" BETWEEN NOW() - INTERVAL '1 DAY' AND now()
+    GROUP BY  category.name) AS sub
 ORDER BY quantidade DESC
 LIMIT 5;
 
@@ -92,9 +94,9 @@ WHERE question."date" BETWEEN $beginning_date AND $end_date AND question.deleted
 -- Obtain all the comments to an answer
 SELECT id_answer, "text", "date", votes, photo, deleted, id_question, user_post
 FROM 
-(SELECT firstAnswer,secondAnswer AS id_comment
-FROM comment
-WHERE firstAnswer = $answer) AS sub
+    (SELECT firstAnswer,secondAnswer AS id_comment
+    FROM comment
+    WHERE firstAnswer = $answer) AS sub
 INNER JOIN answer ON (sub.id_comment = answer.id_answer);
 
 
@@ -106,7 +108,9 @@ ORDER BY ts_rank(search,plainto_tsquery('english',$search)) DESC
 LIMIT 10;
 
 
---------------UPDATES--------------
+-----------------------------------------
+--UPDATES
+-----------------------------------------
  
 -- Update user info
 UPDATE "user"
@@ -122,8 +126,7 @@ SET title = $title , description = $description, name = $name
 WHERE id_question = $id;
 
 
-
--- Update answer/commet
+-- Update answer
 UPDATE answer
 SET "text" = $"text", photo = $photo
 WHERE id_answer = $id;
@@ -141,13 +144,15 @@ SET votes = votes + $quantidade
 WHERE id_question = $id_question;
 
 
--- rate an answer/comment
+-- rate an answer
 UPDATE answer
 SET votes = votes + $quantidade
 WHERE id_answer = $id_answer;
 
 
---------------DELETES--------------
+-----------------------------------------
+--DELETES
+-----------------------------------------
 
 -- delete a question
 UPDATE question
@@ -164,7 +169,10 @@ UPDATE "user"
 SET deleted = true
 WHERE "user".id_user = $id_user;
 
--------------ADD INFO--------------
+
+-----------------------------------------
+--ADD INFO
+-----------------------------------------
 
 -- add new notification to an user
 INSERT INTO notification (id_notification, description, type, view,"date",id_user)
@@ -187,30 +195,25 @@ VALUES(DEFAULT, $title, $description, now(),0, $photo,false,$id_category, $id_us
 
 
 -- add new answer
-INSERT INTO answer(id_answer,"text","date",votes, photo,deleted, id_question, user_post)
-VALUES(DEFAULT,$"text",now(),0, $photo,false, $id_question, $user_post);
+INSERT INTO answer(id_answer,"text", "date", votes, photo,deleted, id_question, user_post)
+VALUES(DEFAULT, $"text", now(), 0, $photo, false, $id_question, $user_post);
 
 -- add new report
-
 INSERT INTO report(id_report,"date", reason, id_question, id_answer)
 VALUES(DEFAULT,now(), $reason, $id_question, $id_answer);
 
--- add new connection user report
-
+-- add new connection between user and report
 INSERT INTO userReport(username,id_report)
 VALUES($username,$id_report);
 
--- add new connection comment
-
+-- add new connection between report and comment
 INSERT INTO userReport(firstAnswer,secondAnswer)
 VALUES($firstAnswer,$secondAnswer);
 
--- add new connection voteAnswer
-
+-- add new connection between Answer and report
 INSERT INTO userReport(username,id_answer)
 VALUES($username,$id_answer);
 
--- add new connection voteQuestion
-
+-- add new connection betweeb report and question
 INSERT INTO userReport(username,id_question)
 VALUES($username,$id_question);
