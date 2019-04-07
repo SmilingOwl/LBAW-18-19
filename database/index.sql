cb27ddca099a500df452b42affd5583ded7c7991
João Augusto Lima <figorifico3000@gmail.com>
2019-04-07 18:36:27 +0100

    2?

--- database/index.sql
+++ database/index.sql
@@ -3,10 +3,13 @@
 --SELECT01 (user profile) , HIGH CARDINALITY, NO CLUSTERING
 
 CREATE INDEX usernames ON "user" USING hash (username);
+--email já tem index porque é um unique key
 
 --SELECT09 (quesTion details), MEDIUM CARDINALITY, CLUSTERING
 
-CREATE INDEX details ON "question" USING hash (id_user);
+--CREATE INDEX details ON "question" USING hash (id_user); 
+--o select09 esta a procurar id_user noutra tabela e não em question
+--as primary keys das tabelas já tem index automatico do postgre
 
 --SELECT10 (question answers), MEDIUM CARDINALITY, CLUSTERING
 
@@ -18,11 +21,8 @@ CREATE INDEX question_date ON "question" USING btree ("date");
 
 --SELECT14 (comments), MEDIUM CARDINALITY, CLUSTERING
 
-CREATE INDEX comments ON "comment" USING hash (firstAnswer);
-
-
+--CREATE INDEX comments ON "comment" USING hash (firstAnswer);
+--Já tem index nas 2 chaves primarias. Index automatico do postgre
 
 --GiST because it's better for dynamic data.
 CREATE INDEX search_idx ON "question" USING GIST (search);
-
-CREATE INDEX search_idx ON "answer" USING GIST (search);
