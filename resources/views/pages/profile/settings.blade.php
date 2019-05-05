bb1098f3930f626d8f625aa528a6cd06588d3a43
SmilingOwl <julianamcm05@gmail.com>
2019-05-05 15:05:52 +0100

    added settings (only delete account)

--- resources/views/pages/profile/settings.blade.php
+++ resources/views/pages/profile/settings.blade.php
@@ -7,6 +7,7 @@
 
     <script src={{asset('js/bar_nav.js')}}></script>
     <script src={{asset('js/profile.js')}}></script>
+    <script src={{asset('js/footer_position.js')}}></script>
 @endsection
 
 
@@ -14,38 +15,7 @@
 
     <div class="container settings-items">
 
-        <div class="row">
-            <h1 style="margin: 5%; text-align: center;">My Account</h1>
-        </div>
-       
-        <div class="row">
-            <h1 style=" margin: 5%; text-align: center;">Connected Accounts</h1>
-        </div>
 
-        <div class="row" style=" margin-bottom: 20px; width: 100%; padding-right: 15px;">
-
-            <div class="row" style="width: 100%; padding-bottom: 10px;">
-                <div class="container-fluid social-buttons align-items-center" style="width:70%;">
-                    <div class="row" style="padding-bottom: 10px;">
-                        <div class="col" >
-                            <a href="#" class="google btn" style="max-width:292px;"><i class=" fa fa-google fa-fw"></i>
-                                Disconnect with Google</a>
-                        </div>
-                        <div class="col" style="min-width:230px;">
-                            <h5 class="text-truncate">luis12mai@gmail.com</h5>
-                        </div>
-
-                    </div>
-                    <div class="row">
-                        <div class="col">
-                            <a href="#" class="fb btn" style="max-width:292px;"><i class="fa fa-facebook fa-fw"></i>
-                                Connect with Facebook</a>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-        </div>
         <div class="row">
             <h1  style=" margin: 5%; padding-right: 15px; text-align: center;">Delete Account</h1>
         </div>
@@ -55,7 +25,7 @@
 
         </div>
         <div class="row">
-            <a href="../pages/feed.html" class="btn btn-dark " style="width: 10em; margin-top: 20px; margin-bottom: 20px;">Delete
+            <a href="{{URL::to('/')}}" class="btn btn-dark " style="width: 10em; margin-top: 20px; margin-bottom: 20px;">Delete
                 Account</a>
         </div>
 
