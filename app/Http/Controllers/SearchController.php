03ec8e0375891fcf00612db549ca2c322277ac7c
Juliana Marques <julianamcm05@gmail.com>
2019-05-14 10:55:41 +0100

    added search controller (not working)

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -6,17 +6,16 @@ class SearchController extends Controller
     {
         // Content type being searched - Questions, topics, Members)
         $type = $request->input('type');
-        $chunk = intval($request->input('chunk'));
         $maxNr = 15;
         $result = [];
 
         switch ($type) {
             case 'questions':
-                $result = $this->getQuestions($query, $chunk, $maxNr);
+                $result = $this->getQuestions($query, $maxNr);
                 break;
 
             case 'members':
-                $result = $this->getMembers($query, $chunk, $maxNr);
+                $result = $this->getMembers($query, $maxNr);
                 break;
             default:
                 break;
@@ -24,25 +23,23 @@ class SearchController extends Controller
         return $result;
     }
 
-    private function getQuestions($search_input,$chunkNr,$maxNr)
+    private function getQuestions($search_input, $maxNr)
     {
         $questions = \App\Models\Question::whereRaw('search @@ to_tsquery(\'english\', replace(plainto_tsquery(\'english\', ?)::text, \'&\', \'|\'))', [$search_input])
             ->orderByRaw('ts_rank(search, to_tsquery(\'english\', replace(plainto_tsquery(\'english\', ?)::text, \'&\', \'|\'))) DESC', [$search_input])
             ->get();
 
-        $res = getDataChunk($questions,$chunkNr,$maxNr);
         $res['data'] = QuestionResource::collection($res['data']);
         return $res;
     }
 
 
 
-    private function getMembers($search_input, $chunkNr, $maxNr)
+    private function getMembers($search_input, $maxNr)
     {
         $members = \App\Models\Member::where('username', 'ILIKE', '%' . $search_input . '%')
             ->orWhere('username', 'ILIKE', '%' . $search_input . '%')
             ->get();
-        $res = getDataChunk($members, $chunkNr, $maxNr);
         $res['data'] = MemberResource::collection($res['data']);
         return $res;
     }
